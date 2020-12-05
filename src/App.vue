38552d96264a48b6a90c05d7f8d445a703b414ba
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 18:18:53 -0500

    fonts 2

--- src/App.vue
+++ src/App.vue
@@ -49,6 +49,9 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Helvetica:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
 #bg {
 	position: absolute;
 	z-index: 0;
