2f3097b701ba6e8034a1166a3e80bfc4e6b74570
Nathan Kim <nathan.kim@shopcanal.com>
2022-07-26 01:28:28 -0700

    cleanup

---
+++ src/routes/blog/tellusboutyourself.svelte
@@ -0,0 +1,83 @@
+<div style="background-color: white; font-family: 'Courier Prime', Courier, monospace">
+    <p style="max-width: 60%">
+        <b>"tellusboutyourself" by Yerin Baek (백예린)</b>
+    </p>
+    <p style="max-width: 60%">
+        Korean-style synthpop record with elements of dance and R&B. K-indie queen Yerin Baek returns
+        for her second full-length album, trading off the wistful keys and guitar ballads of 2019's
+        "Every letter I sent you" for a MIDI-programmed sound that retains her trademark melancholy at
+        its heart. The record's fourteen tracks aren't coy about their influences, ranging from city
+        pop to dancepop to K-R&B, but they do iterate on them with a consistency and warmth which fans
+        of Ms. Baek will find to their liking. Though, once again, listeners in the Anglophone world
+        may find themselves puzzling over the lyricism, whose stiff verbiage sometimes struggles to
+        provide the expressive power demanded of the regret-soaked retrospectives recounted in these
+        tracks.
+    </p>
+    <p style="max-width: 60%">RIYL: The Volunteers, Oohyo</p>
+    <p style="max-width: 60%">FCC No-goes: 1, 10, probably 14</p>
+    <p style="max-width: 60%">Track-by-track (* indicates a fave):</p>
+    <p style="max-width: 60%">
+        1. Lovegame*--Murkey keys overlaid with razor-sharp guitar riffs and synth chords in the high
+        end. Ms. Baek opens her album with a stern warning to the girl who could do better, letting an
+        intense electro-funk keyboard solo take over in the coda as she reminds the listener not to
+        play his "stupid _____ lovegame."
+    </p>
+    <p style="max-width: 60%">
+        2. You're so lonely now, so you need me back by your side again*--Roaring synths in the low
+        end and haunting fuzz guitar creates a spooky ambience that morphs into a solo in the coda.
+    </p>
+    <p style="max-width: 60%">
+        3. I am not your ocean anymore--The roomy snares, elegant keys and yes, sax solos overlaying
+        the slow&steady straight 16ths on this track form the spitting image of a city-pop slow jam.
+    </p>
+    <p style="max-width: 60%">
+        4. Hall&Oates--A rare ballad on the album backed by a minimal keyboard accompaniment.
+    </p>
+    <p style="max-width: 60%">
+        5. Ms Delicate--Another track which wears its city pop influence on its sleeve, albeit with an
+        electro R&B breakdown in the bridge.
+    </p>
+    <p style="max-width: 60%">
+        6. Interlude--clean guitar, low-pass piano and a healthy heaping of reverb.
+    </p>
+    <p style="max-width: 60%">
+        7. Loner--Calm bossa rhythms in the drum machine plus those oh-so-fuzzy key organ chords.
+        Final chorus adds some soothing low-end keys, choral backing vocals and a plinky sample that I
+        just like.
+    </p>
+    <p style="max-width: 60%">
+        8. "HOMESWEETHOME"*--The first of the dancier tracks on this record. Ms. Baek breaks into the
+        chorus with a magic key switch on the synths that paints the dancepop skeleton with a sunny
+        disposition.
+    </p>
+    <p style="max-width: 60%">
+        9. Loveless*--On the outside, another run-of-the-mill K-R&B single--4/4 swung 8ths on the drum
+        machine, plinky key riffs and all--but the sax accompaniment weaving throughout and the
+        extraordinary range Ms. Baek's vocals bring out here give this track its own character.
+    </p>
+    <p style="max-width: 60%">
+        10. Hate you--Thumping kicks and in-your-face synth chords give this track a distinctly
+        #rebellious tone. Under a different lyricist I might have come to enjoy this track but Yerin's
+        not-quite-there delivery now layered with a rather limited vocabulary of insults proved too
+        much for me in the end.
+    </p>
+    <p style="max-width: 60%">
+        11. 0415--A groovy dancepop beat which gets its depth from a syncopated wood-block sample and
+        an entrancing xylophone line in the chorus.
+    </p>
+    <p style="max-width: 60%">
+        12. I'll be your family!--Cozy, chill, city pop-inspired. Glimmering keys, faint guitar lines
+        and Yerin's soft oohs and aahs glide the listener through this track.
+    </p>
+    <p style="max-width: 60%">
+        13. I'm in love--Roomy vocals and straight keyboard chords make for an uplifting slow jam.
+    </p>
+    <p style="max-width: 60%">
+        14. Bubbles&Mushrooms*--Not listed as explicit but features a naughty word near the 45-second
+        mark.Tight, peppy drum beats cut through bubbly synths, whirring chords and cutesy backing
+        vocals (love is a bubble!) in this hip album closer. Filtered kicks pump the listener through
+        the bridge before the comp returns in full force for the final chorus. Ms. Baek's curt and
+        carefree lyrics make this track into a sympathetic acid-trip near-parody of the album's
+        gravity as a whole, and I'm all for it.
+    </p>
+</div>
