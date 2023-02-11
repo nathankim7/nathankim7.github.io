039fc108bfaf130508ae73c71d0672a76660def5
Nathan Kim <nkseve7n@gmail.com>
2023-02-11 15:27:48 -0800

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
 
 const dev = process.env.NODE_ENV === 'development';
 
@@ -10,7 +11,10 @@ const config = {
 
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess(), mdsvex()]
+	preprocess: [preprocess(), mdsvex()],
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
